6bf967e65b32b49a56e2e308557c5f3ba287a53c
XiomarG <gongxun.ad@gmail.com>
2017-05-07 22:10:43 -0700

    redraw advanced view controller move view when keyboard appears

--- src/caffe2-ios/caffe2-ios/advancedTesterVC.swift
+++ src/caffe2-ios/caffe2-ios/advancedTesterVC.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var targetImageView: UIImageView!
     @IBOutlet weak var resultsTextView: UITextView!
     @IBOutlet weak var initNetUrlTextField: UITextField!
@@ -17,6 +18,7 @@ class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImage
     let imagePickerController = UIImagePickerController()
     var pickedImages = [UIImage]()
     let invalidModelName = "invalid"
+    var activeField: UITextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +27,14 @@ class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImage
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap))
         self.view.isUserInteractionEnabled = true
         self.view.addGestureRecognizer(tapGesture)
+        self.initNetUrlTextField.delegate = self
+        self.predictNetUrlTextField.delegate = self
+        self.registerForKeyboardNotifications()
         print("Initializing ...")
+    }
+    
+    deinit {
+        self.deregisterFromKeyboardNotifications()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,7 +50,7 @@ class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImage
         self.view.endEditing(true)
     }
     
-    @IBAction func loadModelsFromUrlBtn(_ sender: UIButton) {
+    @IBAction func downloadModelsFromUrlBtn(_ sender: UIButton) {
         let modelName = self.downloadModels()
         try! caffe.reloadModel(initNetNamed: "\(modelName)Init", predictNetNamed: "\(modelName)Predict")
         print("Switched the model to \(modelName)!")
@@ -94,5 +103,68 @@ class advancedTesterVC: UIViewController,UINavigationControllerDelegate, UIImage
         
     }
     
+    // MARK: Move keyboard view
+    func registerForKeyboardNotifications()
+    {
+        //Adding notifies on keyboard appearing
+        print("registering")
+        NotificationCenter.default.addObserver(self, selector: #selector(advancedTesterVC.keyboardWasShown(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(advancedTesterVC.keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    
+    func deregisterFromKeyboardNotifications()
+    {
+        //Removing notifies on keyboard appearing
+        print("deregistering")
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func keyboardWasShown(notification: NSNotification)
+    {
+        print("keyboard was shown")
+        //Need to calculate keyboard exact size due to Apple suggestions
+        self.scrollView.isScrollEnabled = true
+        let info : NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(65.0, 0.0, keyboardSize!.height + 10, 0.0)
+        
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.scrollView.frame
+        aRect.size.height -= keyboardSize!.height
+        if activeField != nil
+        {
+            if (!aRect.contains(activeField!.frame.origin))
+            {
+                self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification)
+    {
+        //Once keyboard disappears, restore original positions
+        print("keyboard will hide")
+        let info : NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(65.0, 0.0, -keyboardSize!.height - 10, 0.0)
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        self.scrollView.endEditing(true)
+        self.scrollView.isScrollEnabled = false
+    }
+    
+    // MARK: Text field delegate
+    func textFieldDidBeginEditing(_ textField: UITextField)
+    {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        activeField = nil
+    }
 }
 
