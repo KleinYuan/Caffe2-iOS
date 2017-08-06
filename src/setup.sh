01716c10d11c514d3774ddfa68efa01af9dc7864
KleinYuan <kaiwen.yuan1992@gmail.com>
2017-08-06 15:22:13 -0700

    Fixed Caffe2 version

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,10 @@ brew install automake libtool
 echo "[Step2] git cloning Caffe2"
 git clone --recursive https://github.com/caffe2/caffe2.git
 
+echo "[Step2 - b] checkout to a stable version tags/v0.7.0-163-gebc17cc8"
+cd caffe2 && git checkout tags/v0.7.0-163-gebc17cc8
+
 echo "[Step3] build ios"
-cd caffe2 && ./scripts/build_ios.sh
+./scripts/build_ios.sh
 
 echo "[Tada!] Done"
